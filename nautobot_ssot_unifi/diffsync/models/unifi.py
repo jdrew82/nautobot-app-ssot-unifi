3bdabb06059105632fa6f7a173dae1cc4f933a73
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-12-12 08:20:51 -0600

    feat: ✨ Add UniFi CRUD models.

--- nautobot_ssot_unifi/diffsync/models/unifi.py
+++ nautobot_ssot_unifi/diffsync/models/unifi.py
@@ -1,14 +1,112 @@
 """DiffSync models for Nautobot SSoT UniFi."""
 
+from nautobot_ssot_unifi.diffsync.models.base import (
+    LocationModel,
+    ManufacturerModel,
+    RoleModel,
+    DeviceTypeModel,
+    DeviceModel,
+    InterfaceModel,
+)
+
+
+class UniFiLocationModel(LocationModel):
+    """UniFi implementation of Location DiffSync model."""
+
     @classmethod
     def create(cls, diffsync, ids, attrs):
-        """Create Device in Unifi from UnifiDevice object."""
+        """Create Location in UniFi from UniFiLocationModel object."""
         return super().create(diffsync=diffsync, ids=ids, attrs=attrs)
 
     def update(self, attrs):
-        """Update Device in Unifi from UnifiDevice object."""
+        """Update Location in UniFi from UniFiLocationModel object."""
         return super().update(attrs)
 
     def delete(self):
-        """Delete Device in Unifi from UnifiDevice object."""
+        """Delete Location in UniFi from UniFiLocationModel object."""
+        return self
+
+
+class UniFiManufacturerModel(ManufacturerModel):
+    """UniFi implementation of Manufacturer DiffSync model."""
+
+    @classmethod
+    def create(cls, diffsync, ids, attrs):
+        """Create Manufacturer in UniFi from UniFiManufacturerModel object."""
+        return super().create(diffsync=diffsync, ids=ids, attrs=attrs)
+
+    def update(self, attrs):
+        """Update Manufacturer in UniFi from UniFiManufacturerModel object."""
+        return super().update(attrs)
+
+    def delete(self):
+        """Delete Manufacturer in UniFi from UniFiManufacturerModel object."""
+        return self
+
+
+class UniFiDeviceTypeModel(DeviceTypeModel):
+    """UniFi implementation of Manufacturer DiffSync model."""
+
+    @classmethod
+    def create(cls, diffsync, ids, attrs):
+        """Create DeviceType in UniFi from UniFiDeviceTypeModel object."""
+        return super().create(diffsync=diffsync, ids=ids, attrs=attrs)
+
+    def update(self, attrs):
+        """Update DeviceType in UniFi from UniFiDeviceTypeModel object."""
+        return super().update(attrs)
+
+    def delete(self):
+        """Delete DeviceType in UniFi from UniFiDeviceTypeModel object."""
+        return self
+
+
+class UniFiRoleModel(RoleModel):
+    """UniFi implementation of Manufacturer DiffSync model."""
+
+    @classmethod
+    def create(cls, diffsync, ids, attrs):
+        """Create Role in UniFi from UniFiRoleModel object."""
+        return super().create(diffsync=diffsync, ids=ids, attrs=attrs)
+
+    def update(self, attrs):
+        """Update Role in UniFi from UniFiRoleModel object."""
+        return super().update(attrs)
+
+    def delete(self):
+        """Delete Role in UniFi from UniFiRoleModel object."""
+        return self
+
+
+class UniFiDeviceModel(DeviceModel):
+    """UniFi implementation of Interface DiffSync model."""
+
+    @classmethod
+    def create(cls, diffsync, ids, attrs):
+        """Create Device in UniFi from UniFiDevice object."""
+        return super().create(diffsync=diffsync, ids=ids, attrs=attrs)
+
+    def update(self, attrs):
+        """Update Device in UniFi from UniFiDevice object."""
+        return super().update(attrs)
+
+    def delete(self):
+        """Delete Device in UniFi from UniFiDevice object."""
+        return self
+
+
+class UniFiInterfaceModel(InterfaceModel):
+    """UniFi implementation of Device DiffSync model."""
+
+    @classmethod
+    def create(cls, diffsync, ids, attrs):
+        """Create Interface in UniFi from UniFiInterfaceModel object."""
+        return super().create(diffsync=diffsync, ids=ids, attrs=attrs)
+
+    def update(self, attrs):
+        """Update Interface in UniFi from UniFiInterfaceModel object."""
+        return super().update(attrs)
+
+    def delete(self):
+        """Delete Interface in UniFi from UniFiInterfaceModel object."""
         return self

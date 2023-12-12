b8cb1432f761bbbb647c6abf9464a98033be6a9d
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-12-12 08:19:48 -0600

    feat: ✨ Make base DiffSync models based off NautobotModel.

--- nautobot_ssot_unifi/diffsync/models/base.py
+++ nautobot_ssot_unifi/diffsync/models/base.py
@@ -1,28 +1,94 @@
 """DiffSyncModel subclasses for Nautobot-to-UniFi data sync."""
 from typing import Optional
-from uuid import UUID
-from diffsync import DiffSyncModel
+from nautobot_ssot.contrib import NautobotModel
+from nautobot.dcim.models import Device, DeviceType, Interface, Location, Manufacturer
+from nautobot.extras.models import Role
 
 
-class Device(DiffSyncModel):
-    """DiffSync model for Unifi devices."""
+class LocationModel(NautobotModel):
+    """DiffSync model for UniFi locations."""
 
+    _model = Location
+    _modelname = "location"
+    _identifiers = ("name", "location_type__name")
+    _attributes = ("content_types",)
+    _children = {}
+
+    name: str
+    location_type__name: str
+    content_types: str
+
+
+class ManufacturerModel(NautobotModel):
+    """DiffSync model for UniFi client vendors."""
+
+    _model = Manufacturer
+    _modelname = "manufacturer"
+    _identifiers = ("name",)
+    _attributes = ()
+    _children = {}
+
+    name: str
+
+
+class DeviceTypeModel(NautobotModel):
+    """DiffSync model for UniFi device hardware."""
+
+    _model = DeviceType
+    _modelname = "devicetype"
+    _identifiers = ("model", "manufacturer__name")
+    _attributes = ()
+    _children = {}
+
+    model: str
+    manufacturer__name: str
+
+
+class RoleModel(NautobotModel):
+    """DiffSync model for UniFi device roles."""
+
+    _model = Role
+    _modelname = "role"
+    _identifiers = ("name",)
+    _attributes = ("content_types",)
+    _children = {}
+
+    name: str
+    content_types: str
+
+
+class DeviceModel(NautobotModel):
+    """DiffSync model for UniFi devices."""
+
+    _model = Device
     _modelname = "device"
     _identifiers = ("name",)
     _attributes = (
-        "status",
-        "role",
-        "model",
-        "location",
-        "ip_address",
+        "location__name",
+        "serial",
+        "role__name",
+        "devicetype__model",
+        "devicetype__manufacturer__name",
     )
     _children = {}
 
     name: str
-    status: Optional[str]
-    role: Optional[str]
-    model: Optional[str]
-    location: Optional[str]
-    ip_address: Optional[str]
+    location__name: str
+    serial: Optional[str]
+    role__name: Optional[str]
+    devicetype__model: Optional[str]
+    devicetype__manufacturer__name: Optional[str]
 
-    uuid: Optional[UUID]
+
+class InterfaceModel(NautobotModel):
+    """DiffSync model for UniFi device interfaces."""
+
+    _model = Interface
+    _modelname = "interface"
+    _identifiers = ("name",)
+    _attributes = ("type", "mgmt_only")
+    _children = {}
+
+    name: str
+    type: str
+    mgmt_only: bool
